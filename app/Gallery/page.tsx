5fe1badef949c41d5c3a5e6e1bd9b0c8bdb8b287
bharathrnair_ <bharathrnair7@gmail.com>
2025-01-03 17:58:05 +0530

    changes for gallery

---
+++ app/Gallery/page.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+
+export function Gallery(){
+    return(
+<div className="grid gap-4">
+    <div>
+        <Image className="h-auto max-w-full rounded-lg" src={'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg'} alt=""/>
+    </div>
+    <div className="grid grid-cols-5 gap-4">
+        <div>
+            <Image className="h-auto max-w-full rounded-lg" src="" alt=""/>
+        </div>
+        <div>
+            <Image className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt=""/>
+        </div>
+        <div>
+            <Image className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt=""/>
+        </div>
+        <div>
+            <Image className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt=""/>
+        </div>
+        <div>
+            <Image className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt=""/>
+        </div>
+    </div>
+</div>
+
+    );
+}
