e1847c05c2a47e3160cc633cf672f7e00be68a3a
bharathrnair_ <bharathrnair7@gmail.com>
2025-01-06 12:19:59 +0530

    changes for gallery component

--- app/Gallery/page.tsx
+++ app/Gallery/page.tsx
@@ -1,29 +1,64 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-export function Gallery(){
-    return(
-<div className="grid gap-4">
-    <div>
-        <Image className="h-auto max-w-full rounded-lg" src={'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg'} alt=""/>
+const Gallery = () => {
+  return (
+    <div className="grid gap-4">
+      <div>
+        <Image
+          className="h-auto max-w-full rounded-lg"
+          src={
+            "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+          }
+          alt=""
+          width={500}  
+          height={500}
+          layout="responsive"
+        />
+      </div>
+      <div className="grid grid-cols-5 gap-4">
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
+            alt=""
+            width={500} 
+            height={500}
+            layout="responsive"
+          />
+        </div>
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
+            alt=""
+            width={500} 
+            height={500}
+            layout="responsive"
+          />
+        </div>
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
+            alt=""
+            width={500} 
+            height={500}
+            layout="responsive"
+          />
+        </div>
+        <div>
+          <Image
+            className="h-auto max-w-full rounded-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
+            alt=""
+            width={500}  
+            height={500}
+            layout="responsive"
+          />
+        </div>
+      </div>
     </div>
-    <div className="grid grid-cols-5 gap-4">
-        <div>
-            <Image className="h-auto max-w-full rounded-lg" src="" alt=""/>
-        </div>
-        <div>
-            <Image className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt=""/>
-        </div>
-        <div>
-            <Image className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt=""/>
-        </div>
-        <div>
-            <Image className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt=""/>
-        </div>
-        <div>
-            <Image className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt=""/>
-        </div>
-    </div>
-</div>
+  );
+};
 
-    );
-}
+export default Gallery;
